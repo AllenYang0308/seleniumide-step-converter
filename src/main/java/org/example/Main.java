5f8965c9bc19eebf709321e49fa63be1edd457e4
Allen Yang <musasi.yang@gmail.com>
2023-10-13 15:15:53 +0800

    feature: 新增dotenv環境變數存取 將原來Azure DevOps apis url domain擷取為環境變數， 主要是Azure DevOps 可建置於地端，api url domain會變化， 所以存放於環境變數以利變更。

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
-
+import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().filename("env").load();
         try {
             Task task = new Task(args[0]);
-            TestPlansAutomation tpa = new TestPlansAutomation(args[1], args[2]);
+            TestPlansAutomation tpa = new TestPlansAutomation(dotenv.get("AzureApisDomain"), args[1], args[2]);
             String rsp = tpa.CreateSharedParameters(
                     new AzureTestSteps(task).GetAzureSharedParameters(task)
             );
